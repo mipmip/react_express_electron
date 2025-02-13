b0af4e2a0a7d330d435ddf1bd00594f1f458f4e7
Pim Snel <post@pimsnel.com>
2025-02-13 13:32:55 +0100

    fix to run statically as well

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }@inputs:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron}/bin/";
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.electron
+
+
+            # You can set the major version of Node.js to a specific one instead
+            # of the default version
+            # pkgs.nodejs-22_x
+
+            # Comment out one of these to use an alternative package manager.
+            # pkgs.yarn
+            pkgs.pnpm
+            # pkgs.bun
+
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
